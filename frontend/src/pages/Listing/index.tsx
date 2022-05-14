265b394bcb72335c4706779da99366b9d822c613
Mateus Belfort <mateusaguia96@gmail.com>
2022-05-14 15:44:39 -0300

    useParams

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -23,7 +23,7 @@ function Listing() {
     });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
             .then(reponse => {
                 const data = reponse.data as MoviePage;
                 setPage(data);
